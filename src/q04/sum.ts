78c6478e1459610cb0c7d4c1c7acf991db6507a5
Naruchit Choenghom <68010578@kmitl.ac.th>
2025-12-15 09:43:10 +0700

    quiz

---
+++ src/q04/sum.ts
@@ -0,0 +1,18 @@
+export{}
+let result: number = 0;
+let number: number
+
+for (let i = 2; i < process.argv.length; i++) {
+    
+    let input: number = (+process.argv[i]);
+    if (Number.isNaN(input) || input <= 0){
+        console.log('Invalid Input');
+        process.exit();
+    }
+    if (!isNaN(Number(input))) {
+      number = Number(input);
+      result = result + number;
+    }
+}
+
+console.log(`Sum: ${result}`);
