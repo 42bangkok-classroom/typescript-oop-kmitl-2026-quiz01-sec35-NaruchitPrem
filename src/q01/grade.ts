e37bbddbdfcc259727348d794163ba9eb1b16606
Naruchit Choenghom <68010578@kmitl.ac.th>
2025-12-15 09:23:46 +0700

    q01-02

---
+++ src/q01/grade.ts
@@ -0,0 +1,18 @@
+let score: number = Number(process.argv[2]);
+
+
+if(Number.isNaN(score)){
+  console.log('Invalid Input');
+}else if(score < 0 || score > 100){
+  console.log('Invalid Input');
+}else if(score >= 80){
+  console.log('A');
+}else if(score >= 70){
+  console.log('B');
+}else if(score >= 60){
+  console.log('C');
+}else if(score >= 50){
+  console.log('D');
+}else if(score < 50){
+  console.log('F');
+}
---
+++ src/q02/atm.ts
@@ -0,0 +1,20 @@
+let balance: number = +process.argv[2];
+let withdraw: number = +process.argv[3];
+
+if(Number.isNaN(balance)){
+    console.log('Invalid Input');
+    process.exit();
+}
+
+if(Number.isNaN(withdraw)){
+    console.log('Invalid Input');
+    process.exit();
+}
+
+if(withdraw > balance && withdraw <= 5000){
+    console.log('Insufficient balance');
+}else if(withdraw > 5000){
+    console.log('Exceeds per-withdrawal limit');
+}else{
+    console.log('Withdrawal approved');
+}
