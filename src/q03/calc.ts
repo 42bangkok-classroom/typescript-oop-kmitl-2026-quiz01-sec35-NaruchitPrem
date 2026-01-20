b73cff64d846adcde0e332ee6368526fba21bbda
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-20 17:23:33 +0700

    practices

--- src/q03/calc.ts
+++ src/q03/calc.ts
@@ -4,10 +4,14 @@ let num2: number = +process.argv[4];
 let operToLow: string = oper.toLowerCase()
 
 if (process.argv[2] === "" || process.argv[3] === "" || process.argv[1] === "") {
-    console.log('Invalid Input');
+    console.log('Invalid input');
     process.exit();
 }
 
+if (process.argv[2] === undefined || process.argv[3] === undefined || process.argv[1] === undefined) {
+    console.log('Invalid input');
+    process.exit();
+}
 
 if(Number.isNaN(num1) || Number.isNaN(num2)){
     console.log('Invalid input');
@@ -15,7 +19,7 @@ if(Number.isNaN(num1) || Number.isNaN(num2)){
 }
 
 if(operToLow !== 'add' && operToLow !== 'sub' && operToLow !== 'div' && operToLow !== 'mul'){
-    console.log('Invalid input');
+    console.log('Invalid operator');
     process.exit();
 }
 
@@ -23,15 +27,15 @@ let result: number
 
 if(operToLow == 'add'){
     result = num1 + num2;
-    console.log(result)
+    console.log(`${result}`)
 }else if(operToLow == 'sub'){
     result = num1 - num2;
-    console.log(result)
+    console.log(`${result}`)
 }else if(operToLow == 'div'){
     result = num1 / num2;
-    console.log(result)
+    console.log(`${result}`)
 }else if(operToLow == 'mul'){
     result = num1 * num2;
-    console.log(result)
+    console.log(`${result}`)
 }
 
